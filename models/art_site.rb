ba14d4e6401cc0e0e3a3014c276ecdfcf03fe802
Keryn R <kerynjane77@gmail.com>
2016-01-16 12:16:25 +1100

    updated functions and layout

--- models/art_site.rb
+++ models/art_site.rb
@@ -1,6 +1,5 @@
 # maps class to the art_sites table
 
 class ArtSite < ActiveRecord::Base
-  # belongs_to :dish_type
   
 end
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,26 @@
+// function to make scrolling smooth between sections
 
-// create new map object and specific the DOM element for diplay
-// long/lat is Footscray
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+        scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
+
+// creates google map object with location markers
+
 
 function initMap() {
 
-  // (marker: Malcolm 48 Albert St – lat: -37.803182, lng: 144.897727)
+  // marker: Malcolm 48 Albert St
   var myLatlng = {lat: -37.803182, lng: 144.897727 };
 
   // BG horse (-37.798514, 144.889510)
@@ -17,15 +33,25 @@ function initMap() {
   var myLatlng4 = {lat: -37.798474, lng: 144.889435 };
 
   // Elmz street (approx)
-  var myLatlng5 = {lat: -37.801531, lng: 144.900403 };
+  var myLatlng5 = {lat: -37.801947, lng: 144.899882 };
 
+  // stay strong women's day
+  var myLatlng6 = {lat: -37.799132, lng: 144.897426 };
 
+  // Heesco dragon cnr Paisley St
+  var myLatlng7 = {lat: -37.800073, lng: 144.892855 };
+
+  // franco cozzo hopkins st
+  var myLatlng8 = {lat: -37.800105, lng: 144.904036 };
+ 
+  // center long/lat is Footscray
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -37.799736, lng: 144.899734},
+    center: {lat: -37.799132, lng: 144.897426},
     zoom: 15
   } );
 
- // first marker Malcolm Fraser
+
+  // marker 1 Malcolm Fraser
   var infowindow = new google.maps.InfoWindow({
     content: 'Heesco'    
   });
@@ -33,12 +59,13 @@ function initMap() {
   var marker = new google.maps.Marker({
     position: myLatlng,
     map: map,
-    title: 'Heesco'
+    title: 'Malcolm Fraser'
   })
 
   marker.addListener('click', function(){
     infowindow.open(map, marker);
   });
+
 
   // BG horse marker 2
   var infowindow2 = new google.maps.InfoWindow({
@@ -48,31 +75,33 @@ function initMap() {
   var marker2 = new google.maps.Marker({
     position: myLatlng2,
     map: map,
-    title: 'Baby Guerrilla'
+    title: 'In memoriam'
   })
 
   marker2.addListener('click', function(){
     infowindow2.open(map, marker2);
   });
 
+
   // BG VU marker 3
   var infowindow3 = new google.maps.InfoWindow({
-    content: 'Baby Guerrilla 2'    
+    content: 'Baby Guerrilla'    
   });
 
   var marker3 = new google.maps.Marker({
     position: myLatlng3,
     map: map,
-    title: 'Baby Guerrilla 2'
+    title: 'Big floating woman'
   })
 
   marker3.addListener('click', function(){
     infowindow3.open(map, marker3);
   });
 
-// Pirate cat marker 4
+
+  // Pirate cat marker 4
   var infowindow4 = new google.maps.InfoWindow({
-    content: 'Unknown'    
+    content: 'Unknown artist'    
   });
 
   var marker4 = new google.maps.Marker({
@@ -86,7 +115,7 @@ function initMap() {
   });
 
 
-// Elmz street marker 5 (approx location) -37.801531, 144.900403
+  // Elmz street marker 5 
   var infowindow5 = new google.maps.InfoWindow({
     content: 'Elmz Street'    
   });
@@ -94,24 +123,141 @@ function initMap() {
   var marker5 = new google.maps.Marker({
     position: myLatlng5,
     map: map,
-    title: 'approx location only'
+    title: 'Face mural'
   })
 
   marker5.addListener('click', function(){
     infowindow5.open(map, marker5);
   });
-}
+
+
+  // stay strong 6
+  var infowindow6 = new google.maps.InfoWindow({
+    content: 'Trashort Art'    
+  });
+
+  var marker6 = new google.maps.Marker({
+    position: myLatlng6,
+    map: map,
+    title: 'Stay Strong mural'
+  })
+
+  marker6.addListener('click', function(){
+    infowindow6.open(map, marker6);
+  });
+
+
+  // dragon 7 
+  var infowindow7 = new google.maps.InfoWindow({
+    content: 'Heesco'    
+  });
+
+  var marker7 = new google.maps.Marker({
+    position: myLatlng7,
+    map: map,
+    title: 'Chinese Dragon'
+  })
+
+  marker7.addListener('click', function(){
+    infowindow7.open(map, marker7);
+  });
+
+
+  // franco cozzo 8 
+  var infowindow8 = new google.maps.InfoWindow({
+    content: 'Heesco'    
+  });
+
+  var marker8 = new google.maps.Marker({
+    position: myLatlng8,
+    map: map,
+    title: 'Franco Cozzo mural'
+  })
+
+  marker8.addListener('click', function(){
+    infowindow8.open(map, marker8);
+  });
+
+
+// to change marker colour of selected artsite at click on artist name
+// and scroll to map
+
+var changeRed = function() {
+    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    marker5.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    marker4.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    marker3.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    marker7.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    marker6.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    marker8.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+};
+
+
+  $(".art-7").click(function() {
+    changeRed();
+    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+    $('html, body').animate({
+      scrollTop: $("#map").offset().top
+    }, 1000);
+  });
+
+  $(".art-6").click(function() {
+    changeRed();
+    marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+    $('html, body').animate({
+      scrollTop: $("#map").offset().top
+    }, 1000);
+  });
+
+  $(".art-8").click(function() {
+      changeRed();
+      marker5.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+      $('html, body').animate({
+      scrollTop: $("#map").offset().top
+    }, 1000);
+    });
+
+  $(".art-9").click(function() {
+      changeRed();
+      marker4.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+      $('html, body').animate({
+      scrollTop: $("#map").offset().top
+    }, 1000);
+    });
+
+  $(".art-5").click(function() {
+      changeRed();
+      marker3.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+      $('html, body').animate({
+      scrollTop: $("#map").offset().top
+    }, 1000);
+    });
+
+  $(".art-10").click(function() {
+      changeRed();
+      marker7.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+      $('html, body').animate({
+      scrollTop: $("#map").offset().top
+    }, 1000);
+    });
+
+  $(".art-11").click(function() {
+      changeRed();
+      marker6.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+      $('html, body').animate({
+      scrollTop: $("#map").offset().top
+    }, 1000);
+    });
+
+  $(".art-12").click(function() {
+      changeRed();
+      marker8.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+      $('html, body').animate({
+      scrollTop: $("#map").offset().top
+    }, 1000);
+  });
+
+};
 
 initMap();
-
-
-$(document).ready(function() {
-  $(".fancybox").fancybox();
-});
-
-
-
-// Elmz street (unknown location)
-
-// To add the marker to the map, call setMap();
-// marker.setMap(map);
